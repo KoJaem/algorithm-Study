1c9e377a58a68e91de71e96e67d2e1d49b5c6716
KoJaem <woalswhwh@naver.com>
2022-06-25 01:56:47 +0900

    solve : 완주하지 못한 선수 풀이

--- programmers/완주하지 못한 선수.js
+++ programmers/완주하지 못한 선수.js
@@ -1,3 +1,39 @@
+// 문제의 핵심은 '한명의 선수'를 제외하고는 모든 선수가 완주.
+// 문제풀때 핵심정리를 잘하고 풀어야할듯.
+
+const participant = ["mislav", "stanko", "mislav", "ana"];
+const completion = ["stanko", "ana", "mislav"];
+
+function solution(participant, completion) {
+  participant.sort();
+  completion.sort();
+  let answer = '';
+  for(let i = 0; i < participant.length; ++i) {
+    if(participant[i] !== completion[i]) {
+      answer += participant[i];
+      break;
+    }
+  }
+
+  return answer;
+}
+
+solution(participant, completion);
+
+/////////////////////////////////////////////////////////
+
+// const participant = ["mislav", "stanko", "mislav", "ana"];
+// const completion = ["stanko", "ana", "mislav"];
+
+// function solution(participant, completion) {
+//   const answer = String(participant.filter(data => {
+//     if (!completion.includes(data)) return data;
+//     else completion[completion.indexOf(data)] = -1;
+//   }));
+//   return answer;
+// }
+// solution(participant, completion);
+
 /////////////////////////////////////////////////////////
 // // 아래방법에서 동명이인을 걸러낸다해도 효율성테스트 통과 X
 
