58288fa917a0e5f30cb7aaf2e9714b2042fb1b09
KoJaem <woalswhwh@naver.com>
2022-06-27 02:05:12 +0900

    solve : 체육복 문제 해결

--- programmers/체육복.js
+++ programmers/체육복.js
@@ -1,21 +1,27 @@
-const n = 5;
-const lost = [2, 4];
-const reserve = [1, 3, 6, 5];
+// 테스트 케이스를 내가 만들어서 실행해보는것이 도움이 됐음.
+
+const n = 10;
+const lost = [1, 2, 3, 4, 5, 6, 9, 10];
+const reserve = [1, 2, 3, 4, 5, 6, 7, 10];
 
 function solution(n, lost, reserve) {
-  lost.sort((a,b) => a-b);
-  reserve.sort((a,b) => a-b);
+  lost.sort((a, b) => a - b);
+  reserve.sort((a, b) => a - b);
   let answer = n - lost.length;
   let includeIdx;
-  for(let i = 0; i < lost.length; ++i) {
+
+  for (let i = 0; i < lost.length; ++i) {
     includeIdx = reserve.indexOf(lost[i]);
     if (includeIdx !== -1) {
       answer++;
+      lost[i] = null;
       reserve[includeIdx] = null;
       continue;
     }
-    for(let j = 0; j < reserve.length; ++j) {
-      if(Math.abs(lost[i] - reserve[j]) <= 1) {
+  }
+  for (let i = 0; i < lost.length; ++i) {
+    for (let j = 0; j < reserve.length; ++j) {
+      if (reserve[j] !== null && Math.abs(lost[i] - reserve[j]) <= 1) {
         answer++;
         reserve[j] = null;
         break;
