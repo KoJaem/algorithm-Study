8b2faa5ab9a3e96661772df74e8f614177fbe6ff
KoJaem <woalswhwh@naver.com>
2022-07-02 00:22:39 +0900

    fix 중복확인 할때 값을 넣고 필터하는 방법 -> 값을 넣기전에 확인하는 방법으로 변경

--- programmers/두 개 뽑아서 더하기.js
+++ programmers/두 개 뽑아서 더하기.js
@@ -1,14 +1,14 @@
 const numbers = [2, 1, 3, 4, 1];
 function solution(numbers) {
-  let answer = [];
+  const answer = [];
   for(let i = 0; i < numbers.length-1; ++i) {
     for(let j = i+1; j < numbers.length; ++j) {
-      answer.push(numbers[i]+numbers[j]);
+      const data = numbers[i] + numbers[j];
+      if (!answer.includes(data)) {
+        answer.push(data);
+      }
     }
   }
-  answer = answer.filter((num, index) => (
-    answer.indexOf(num) === index
-  ))
   answer.sort((a,b) => a-b);
   return answer;
 }
