65bacb1ff6c488a0a98e978426693e4ef7148c5c
KoJaem <woalswhwh@naver.com>
2022-07-21 03:42:49 +0900

    add : 수박문제 repeat 함수 내용 추가

--- programmers/수박수박수박수박수박수.js
+++ programmers/수박수박수박수박수박수.js
@@ -1,11 +1,22 @@
+// String.prototype.repeat() : 문자열을 주어진 횟수만큼 반복해 붙인 새로운 문자열을 반환
 const n = 3;
 
 function solution(n) {
-  let answer = "";
-  for(let i = 0; i < n; ++i) {
-    i % 2 === 0 ? answer += "수" : answer+= "박"
-  }
-  return answer;
+  return "수박".repeat(n / 2 + 1).slice(0, n);
 }
 
 solution(n);
+
+/////////////////////////////////////////////////////
+// 원래코드
+// const n = 3;
+
+// function solution(n) {
+//   let answer = "";
+//   for(let i = 0; i < n; ++i) {
+//     i % 2 === 0 ? answer += "수" : answer+= "박"
+//   }
+//   return answer;
+// }
+
+// solution(n);
