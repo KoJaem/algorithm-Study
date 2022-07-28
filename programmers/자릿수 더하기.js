853da587a944461677871fd34745f79152ac14c4
KoJaem <woalswhwh@naver.com>
2022-07-28 21:28:30 +0900

    chore : 오타수정

--- programmers/자릿수 더하기.js
+++ programmers/자릿수 더하기.js
@@ -3,9 +3,9 @@ const n = 123;
 function solution(n)
 {
   let answer = 0;
-  const array = n.toString();
-  for(let i = 0; i < array.length; ++i) {
-    answer += Number(array[i]);
+  const str = n.toString();
+  for(let i = 0; i < str.length; ++i) {
+    answer += Number(str[i]);
   }
   return answer;
 }
