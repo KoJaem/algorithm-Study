253b5028462e61f7c3a8f9610efe22c0ed456e31
KoJaem <woalswhwh@naver.com>
2022-08-01 01:40:23 +0900

    add : 유클리드 호제법 개념 추가

--- programmers/최대공약수와 최소공배수.js
+++ programmers/최대공약수와 최소공배수.js
@@ -3,18 +3,24 @@ const m = 12;
 
 function solution(n, m) {
   let maxFactor = 1;
-  let minMultiple = 0;
-  
-  for(let i = 1; i <= Math.min(n,m); ++i) {
-    if(n % i === 0 && m % i === 0) {
+  let minMultiple;
+
+  // 최대 공약수
+  for (let i = 1; i <= Math.min(n, m); ++i) {
+    if (n % i === 0 && m % i === 0) {
       maxFactor = i;
     }
   }
-  for(let i = n*m; i >= Math.min(n,m); --i) {
-    if(i % n === 0 && i % m ===0) {
-      minMultiple = i;
-    }
-  }
+
+  // 최소 공배수
+  // for(let i = n*m; i >= Math.min(n,m); --i) {
+  //   if(i % n === 0 && i % m ===0) {
+  //     minMultiple = i;
+  //   }
+  // }
+
+  // 유클리드 호제법 (A,B의 최소공배수) = A * B / (A,B의 최대공약수)
+  minMultiple = n*m / maxFactor;
 
   return [maxFactor, minMultiple];
 }
