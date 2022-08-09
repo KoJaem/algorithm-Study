379de76bc98e90d52a74f46a78356aa4d926d558
KoJaem <woalswhwh@naver.com>
2022-08-09 19:35:25 +0900

    solve : 문자열 압축 문제 해결

---
+++ programmers/Lv2/문자열 압축.js
@@ -0,0 +1,42 @@
+const s = "a";
+function solution(s) {
+  let answer = 1001;
+  let array = [];
+  let string = "";
+
+  for (let i = 0; i < s.length; ++i) {
+    let start = 0;
+    let count = i + 1;
+    let j = i + 1;
+    array = [];
+    string = "";
+
+    while (j < s.length + count) {
+      array.push(s.slice(start, j));
+      j += count;
+      start += count;
+    }
+
+    count = 1;
+
+    for (j = 0; j < array.length; ++j) {
+      if (array[j] === array[j + 1]) {
+        count++;
+      } else {
+        if (count === 1) {
+          string += array[j];
+        } else {
+          string += count + array[j];
+        }
+        count = 1;
+      }
+    }
+    if (answer > string.length) answer = string.length;
+  }
+
+  console.log(answer);
+  return answer;
+}
+
+solution(s);
+// console.log(solution(s));
