b5bc67e3a38b9e387d72f7cfcc74071e89abe559
KoJaem <woalswhwh@naver.com>
2022-11-03 00:59:52 +0900

    solve : 성격 유형 검사하기 문제 해결

--- programmers/Lv1/성격 유형 검사하기.js
+++ programmers/Lv1/성격 유형 검사하기.js
@@ -15,21 +15,26 @@ function solution(survey, choices) {
   };
 
   
+  for(let i = 0; i < survey.length; ++i) {
+    const first = survey[i].slice(0, 1) // 첫번째 문자
+    const second = survey[i].slice(1); // 두번째 문자
 
-  for (let i = 0; i < survey.length; ++i) {
     const score = choices[i] - 4;
-    if (score === 0) continue;
 
+    if (score === 0) continue;
+    
     if (score > 0) {
-      obj[survey[i].slice(1)] += score;
+      obj[second] += score;
     } else {
-      obj[survey[i].slice(0, 1)] += -score;
+      obj[first] += -score;
     }
   }
-  console.log(obj)
-  // const sortObj = Object.entries(obj).sort((a, b) => b[1] - a[1]);
 
-
+  answer += obj.R >= obj.T ? 'R' : 'T';
+  answer += obj.C >= obj.F ? 'C' : 'F';
+  answer += obj.J >= obj.M ? 'J' : 'M';
+  answer += obj.A >= obj.N ? "A" : "N";
+  
 
   return answer;
 }
